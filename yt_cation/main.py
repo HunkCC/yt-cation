abd6f6bddd961319ccabf4455dc480c9093063f1
HunkCC <basic168@gmail.com>
2022-06-19 10:36:06 +0800

    Initial commit

---
+++ yt_cation/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@Project: yt-cation
+@file: main.py 
+@Create time: 2022-06-19 09:31
+@IDE: PyCharm
+@Creator: CHC
+@Introduction:
+
+
+# Life is short, you need Pythons.
+"""
+
+if __name__ == "__main__":
+    pass
